c4828a5345712850e09fc5137ba2570fe7cc06eb
Elen Damares <elen.damares774@gmail.com>
2024-10-27 15:25:23 -0300

    remover Set_FeaturedPlaylist

--- src/pages/Dashboard/components/Set_FeaturedPlaylist.tsx
+++
@@ -1,9 +0,0 @@
-const Set_FeaturedPlaylist = () => {
-    return(
-        <form>
-            <label>Informe o link da sua playlist</label>
-            <input type="url" name="playlist-url"></input>
-            <button type="submit"></button>
-        </form>
-    )
-}
