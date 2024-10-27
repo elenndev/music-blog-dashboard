e0cea72e02a115ccc183ce80f0cf27e5f23ed1be
Elen Damares <elen.damares774@gmail.com>
2024-10-27 18:01:11 -0300

    correcao na rota principal

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const home = document.querySelector('header>nav>a.home-link')
 const about_me = document.querySelector('header>nav>a.aboutMe-link')
-if (window.location.pathname == "/") {
+if (window.location.pathname === "/") {
     if(about_me.classList.contains('current-page')){
         about_me.classList.remove('current-page')
     }
     home.classList.add('current-page')
-} else {
-    if (window.location.pathname == "/sobre-mim"){
-        home.classList.remove('current-page')
-    }
 }
 
 if (window.location.pathname == "/sobre-mim"){
