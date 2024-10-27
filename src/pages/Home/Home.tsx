d13f4d81eb2e1f225adcbd36120fc14701166c99
Elen Damares <elen.damares774@gmail.com>
2024-10-27 14:59:10 -0300

    botao back to top

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Container_LatestPosts from "./components/Container_LatestPosts";
 import '../Home/components/static/Home.css'
 import Aside from "./components/Aside";
+import BackToTop from "../../components/Button_BackToTop";
 const Home = () => {
     
     return(
         <main id="main">
             <Container_LatestPosts />
             <Aside />
+            <BackToTop/>
         </main>
     )
 }
