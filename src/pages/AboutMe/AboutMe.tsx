ae761610640540e8f0cdc76817021b5986233801
elen work <elen.damares774@gmail.com>
2024-10-28 13:07:39 -0300

    mudar container do aboutMe de div pra main

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -10,7 +10,7 @@ import musicboardLogo from "../../components/musicboard.png"
 
 const AboutMe = () => {
     return(
-        <div className="container about-me">
+        <main className="container about-me">
             <img role="none" className='profile' src={profileIcon}></img>
             <h1>Boas vindas ao meu blog !</h1>
             <p className='info'>Criei este blog com o intuito de falar sobre música e principalmente colocar em prática os meus estudos em programação enquanto o desenvolvo, além de também conhecer pessoas de ambas as áreas para se conectar! Se você quiser, fique à vontade para puxar um papo seja sobre música, programação, suas opiniões, sugestões etc. <br></br>Mais informações nos links abaixo:</p>
@@ -21,7 +21,7 @@ const AboutMe = () => {
                 <a aria-label="Abrir perfil do musicboard" className='btn btn-primary transparent'><img alt="logo do musicboard" className='musicboard' src={musicboardLogo}></img>MusicBoard</a>
                 <a aria-label="Abrir perfil do lastfm" href="https://www.last.fm/user/GOTHMIKASA" target="_blank" rel="noopener norferrer" className='btn btn-primary transparent'><SVG_lastFm/>LastFm </a>
             </span>
-        </div>
+        </main>
     )
 }
 export default AboutMe
