14fa0e9df7d28a205d4ab103db36ab8635604f13
Elen Damares <elen.damares774@gmail.com>
2024-10-29 00:10:25 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/components/Type_BlogSaves.tsx
+++
@@ -1,6 +0,0 @@
-type Type_BlogSave = {
-    text_value: string;
-    // Outras propriedades, se existirem
-};
-
-export default Type_BlogSave
--- src/pages/Dashboard/components/static/submiteFeaturedPlaylist.js
+++ src/pages/Dashboard/components/static/submiteFeaturedPlaylist.js
@@ -1,11 +1,37 @@
+import supabase from '../../../../components/static/auth';
+
 function submitFeaturedPlaylist(event){
     event.preventDefault()
     let input = document.querySelector('div.featured-playlist>form>input').value
     const embed_link = 'https://open.spotify.com/embed/playlist/'
     const playlist = embed_link + input.split('playlist/')[1]
-    localStorage.setItem('featuredPlaylist', playlist)
+    // localStorage.setItem('featuredPlaylist', playlist)
+
     
-    document.querySelector('div.featured-playlist>form>input').value = ""
+    const submit = async() =>{
+        const {data: {user}} = await supabase.auth.getUser();
+        if (user.error) {
+            console.error('Erro ao obter usuário:', user.error);
+            return;
+        }
+        if (user.aud == 'authenticated'){
+            console.log('autenticado')
+            // const text_value = playlist
+            const data = {
+                text_value: playlist
+            }
+            const {error: updateError, status, statusText} = await supabase.from('blog-saves').update({text_value: playlist}).eq("id",1).select()
+            console.log('status: ',statusText)
+            if(updateError){
+                console.log(updateError)
+            }
+            document.querySelector('div.featured-playlist>form>input').value = ""
+        }
+
+
+    }
+    
+    submit()
 }
 
 export default submitFeaturedPlaylist
