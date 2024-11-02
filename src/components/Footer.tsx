c19cfef38535731f5c47336870fcbfb5a81ea99a
Elen Damares <elen.damares774@gmail.com>
2024-11-01 21:29:05 -0300

    correcao no css dos links e botoes do about me

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <ul>
-                <li><p>2024 Music Archive</p></li>
+                <li className="pageName"><p>2024 Music Archive</p></li>
                 <li><a href="#"><p>Email: elen.damares774@gmail.com</p></a></li>
                 <li><a href="/sobre-mim" className="page-about-me"><p>Sobre</p></a></li>
                 <li><a href="https://www.svgrepo.com" target="_blank">Vectors and icons by SVG Repo</a></li>
